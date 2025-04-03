c0403cb3d0884903549891986792eb7de040e8b6
pbh914 <52387364+ArkBB@users.noreply.github.com>
2025-04-03 21:31:46 +0900

    first commit

---
+++ src/test/java/com/example/bhboard/BhBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bhboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
